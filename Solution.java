c5485a96e0447bcb6dda7f943a0f578416874b75
ehm <emrahhalacoglu@gmail.com>
2022-10-27 14:09:23 +0300

    code blocks has been reorganised.

--- Solution.java
+++ Solution.java
@@ -12,12 +12,15 @@ public class Solution {
                 {
                     long x=sc.nextLong();
                     System.out.println(x+" can be fitted in:");
-                    if(x>=-128 && x<=127)System.out.println("* byte");
-                    if(x>=-32768 && x<=32767)System.out.println("*short");
-                    if(x>=-Math.pow(2,31) && x<=Math.pow(2,31)-1)System.out.println("*int");
-                    if(x>=-Math.pow(2,63) && x<=Math.pow(2,63)-1)System.out.println("*long");
+                    if(x>=-128 && x<=127){
+                        System.out.println("* byte");}
+                    if(x>=-32768 && x<=32767){
+                        System.out.println("*short");}
+                    if(x>=-Math.pow(2,31) && x<=Math.pow(2,31)-1){
+                        System.out.println("*int");}
+                    if(x>=-Math.pow(2,63) && x<=Math.pow(2,63)-1){
+                        System.out.println("*long");}
                     
-                    //Complete the code
                 }
                 catch(Exception e)
                 {
